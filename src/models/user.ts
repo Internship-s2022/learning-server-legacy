de0f712410fd9c211d167d94cb78397bffe01751
agustinLchazarreta <agustin.chazarreta@radiumrocket.com>
2022-09-23 12:28:00 -0300

    RL-29-30:setup mongo and setup mongoose

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,4 +1,7 @@
-const userSchema = {
+import mongoose from 'mongoose';
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
   firstName: {
     required: true,
   },
@@ -11,6 +14,5 @@ const userSchema = {
   password: {
     required: true,
   },
-};
-
-export default userSchema;
+});
+export default mongoose.model('User', userSchema);
