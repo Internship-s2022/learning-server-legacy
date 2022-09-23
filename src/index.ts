19fa4b4d9d79284bcfe318e3ad5de1f65c65b7b0
agustinLchazarreta <agustin.chazarreta@radiumrocket.com>
2022-09-23 12:39:26 -0300

    RL-31:change index in src conecting mongodb and mongoose

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,24 @@
 import 'express-async-errors';
 import dotenv from 'dotenv';
+import mongoose from 'mongoose';
 
 import app from './app';
 
 dotenv.config();
 
-const port = process.env.PORT || 4000;
+const port = process.env.PORT;
+const MONGODB_URL = process.env.MONGO_URL || '';
 
-app.listen(port, () => {
-  console.log(`⚡️ Server is running at http://localhost:${port} ✅`);
+mongoose.connect(MONGODB_URL, (error) => {
+  if (error) {
+    // eslint-disable-next-line no-console
+    console.log('🔴 Database error: ', error);
+  } else {
+    // eslint-disable-next-line no-console
+    console.log('✅ Database connected');
+    app.listen(port, () => {
+      // eslint-disable-next-line no-console
+      console.log(`Example app listening on port ${port}`);
+    });
+  }
 });
