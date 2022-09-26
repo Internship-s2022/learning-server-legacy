f67a8c15b1d05601402a5133cdab6bf70b62a52a
agustinLchazarreta <agustin.chazarreta@radiumrocket.com>
2022-09-26 11:46:29 -0300

    RL-23- implement schemma

---
+++ src/data/super-admin.ts
@@ -0,0 +1,23 @@
+export default [
+  {
+    firebaseUid: 'asdasd123',
+    firstName: 'Gustavo',
+    lastName: 'Loreal',
+    email: 'gustavo.loreal@radiumrocket.com',
+    isActive: true,
+  },
+  {
+    firebaseUid: 'asdasd123',
+    firstName: 'Facundo',
+    lastName: 'Garoto',
+    email: 'facundo.garoto@radiumrocket.com',
+    isActive: true,
+  },
+  {
+    firebaseUid: 'asdasd123',
+    firstName: 'Pablo',
+    lastName: 'Gomez',
+    email: 'pablo.gomez@radiumrocket.com',
+    isActive: false,
+  },
+];
