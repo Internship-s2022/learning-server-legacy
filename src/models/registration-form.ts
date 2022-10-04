e49ea041ab16991aa5885297219fceeae762dd83
Radium Rocket - N 17 <guido.cerioni@radiumrocket.com>
2022-10-04 10:23:55 -0300

    RL-25: Add registration form model and validations

---
+++ src/models/registration-form.ts
@@ -0,0 +1,37 @@
+import mongoose from 'mongoose';
+
+const { Schema } = mongoose;
+
+const registrationFormSchema = new Schema(
+  {
+    course_id: {
+      type: String,
+      required: true,
+    },
+
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: false,
+    },
+    views: [
+      {
+        id: { type: String, required: true },
+        name: { type: String, required: true },
+      },
+    ],
+    isActive: {
+      type: Boolean,
+      required: true,
+      default: true,
+    },
+  },
+  { timestamps: true },
+);
+
+export type RegistrationFormTypes = mongoose.InferSchemaType<typeof registrationFormSchema>;
+
+export default mongoose.model('RegistrationForm', registrationFormSchema);
---
+++ src/routes/registration-form/validations.ts
@@ -0,0 +1,55 @@
+import { NextFunction, Request, Response } from 'express';
+import Joi from 'joi';
+
+const registrationFormValidation = (req: Request, res: Response, next: NextFunction) => {
+  const registrationFormValidation = Joi.object({
+    course_id: Joi.string()
+      .pattern(/^[0-9a-fA-F]{24}$/)
+      .required()
+      .messages({
+        'string.pattern.base': 'Invalid ObjectId, it must be a valid MongoId',
+        'any.required': 'Course id is a required field',
+      }),
+    title: Joi.string().min(3).max(50).required().messages({
+      'string.min': 'Invalid title, it must contain more than 3 letters',
+      'string.max': 'Invalid title, it must not contain more than 50 letters',
+      'any.required': 'Title is a required field',
+    }),
+    description: Joi.string()
+      .pattern(/(.*[a-zA-Z]){4}/)
+      .required()
+      .messages({
+        'string.pattern.base': 'Invalid description, it must contain at least 4 letters',
+        'any.required': 'Description is a required field',
+      }),
+    views: Joi.array()
+      .items(
+        Joi.object({
+          id: Joi.number().required(),
+          name: Joi.string().min(3).max(24).required(),
+        }),
+      )
+      .required()
+      .messages({
+        'string.max': 'Invalid type, it must not contain more than 24 characters',
+        'string.min': 'Invalid title, it must contain more than 3 characters',
+        'any.required': 'Missing required data',
+      }),
+    isActive: Joi.boolean().required().messages({
+      'any.required': 'Is active is a required field',
+    }),
+  });
+
+  const validation = registrationFormValidation.validate(req.body);
+
+  if (validation.error) {
+    return res.status(400).json({
+      message: 'There has been an error in the validation',
+      data: validation.error.details[0].message,
+      error: true,
+    });
+  }
+  return next();
+};
+
+export default { registrationFormValidation };
