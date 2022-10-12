7ba0b1fd94d4ef8927f8aaec763492e4506b0a2c
Radium Rocket - N 17 <guido.cerioni@radiumrocket.com>
2022-10-12 09:39:13 -0300

    RL-82: Update RegistrationForm controllers, now with CustomError

--- src/routes/registration-form/controllers.ts
+++ src/routes/registration-form/controllers.ts
@@ -1,152 +1,95 @@
 import { Request, Response } from 'express';
 
+import { CustomError } from 'src/models/custom-error';
+
 import RegistrationForm, { RegistrationFormTypes } from '../../models/registration-form';
 
 const getAll = async (req: Request, res: Response) => {
-  try {
-    if (req.query) {
-      const allRegistrationForms = await RegistrationForm.find(req.query);
-      if (allRegistrationForms.length) {
-        return res.status(200).json({
-          message: 'Showing the list of registration forms',
-          data: allRegistrationForms,
-          error: false,
-        });
-      }
-      return res.status(404).json({
-        message: 'Cannot find the list of registration forms.',
-        data: undefined,
-        error: true,
-      });
-    }
-    const allRegistrationForms = await RegistrationForm.find({});
-    if (allRegistrationForms.length) {
-      return res.status(200).json({
-        message: 'Showing all the registration forms',
-        data: allRegistrationForms,
-        error: false,
-      });
-    }
-    return res.status(404).json({
-      message: 'Cannot find the list of all registration forms.',
-      data: undefined,
-      error: true,
-    });
-  } catch (error: any) {
-    return res.status(500).json({
-      message: `Something went wrong: ${error.message}`,
-      data: undefined,
-      error: true,
+  const registrationForms = await RegistrationForm.find(req.query);
+  if (registrationForms.length) {
+    return res.status(200).json({
+      message: 'Showing the list of registration forms',
+      data: registrationForms,
+      error: false,
     });
   }
+  throw new CustomError(404, 'Cannot find the list of registration forms.', undefined);
 };
 
 const getById = async (req: Request, res: Response) => {
-  try {
-    const registrationForm = await RegistrationForm.findById(req.params.id);
-    if (!registrationForm) {
-      return res.status(404).json({
-        message: `Registration form with id ${req.params.id} was not found.`,
-        data: undefined,
-        error: true,
-      });
-    }
+  const registrationForm = await RegistrationForm.findById(req.params.id);
+  if (!registrationForm) {
     return res.status(200).json({
       message: 'The registration form has been successfully found',
       data: registrationForm,
       error: false,
     });
-  } catch (error: any) {
-    return res.status(500).json({
-      message: `Something went wrong: ${error.message}`,
-      data: undefined,
-      error: true,
-    });
   }
+  throw new CustomError(
+    404,
+    `Registration form with id ${req.params.id} was not found.`,
+    undefined,
+  );
 };
 
 const create = async (req: Request, res: Response) => {
-  try {
-    const registrationForm = new RegistrationForm<RegistrationFormTypes>({
-      course_id: req.body.course_id,
-      title: req.body.title,
-      description: req.body.description,
-      views: req.body.views,
-      isActive: req.body.isActive,
-    });
-    await registrationForm.save();
-    return res.status(201).json({
-      message: 'Registration form successfully created.',
-      data: registrationForm,
-      error: false,
-    });
-  } catch (error: any) {
-    return res.status(500).json({
-      message: `Something went wrong: ${error.message}`,
-      data: undefined,
-      error: true,
-    });
-  }
+  const registrationForm = new RegistrationForm<RegistrationFormTypes>({
+    course_id: req.body.course_id,
+    title: req.body.title,
+    description: req.body.description,
+    views: req.body.views,
+    isActive: req.body.isActive,
+  });
+  await registrationForm.save();
+  return res.status(201).json({
+    message: 'Registration form successfully created.',
+    data: registrationForm,
+    error: false,
+  });
 };
 
 const updateById = async (req: Request, res: Response) => {
-  try {
-    const updatedRegistrationForm = await RegistrationForm.findByIdAndUpdate(
-      req.params.id,
-      req.body,
-      {
-        new: true,
-      },
-    );
-    if (!updatedRegistrationForm) {
-      return res.status(404).json({
-        message: `Registration form with id ${req.params.id} was not found.`,
-        data: undefined,
-        error: true,
-      });
-    }
+  const updatedRegistrationForm = await RegistrationForm.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    {
+      new: true,
+    },
+  );
+  if (updatedRegistrationForm) {
     return res.status(200).json({
       message: 'The registration form has been successfully updated.',
       data: updatedRegistrationForm,
       error: false,
     });
-  } catch (error: any) {
-    return res.status(500).json({
-      message: `Something went wrong: ${error.message}`,
-      data: undefined,
-      error: true,
-    });
   }
+  throw new CustomError(
+    404,
+    `Registration form with id ${req.params.id} was not found.`,
+    undefined,
+  );
 };
 
 const deleteById = async (req: Request, res: Response) => {
-  try {
-    const result = await RegistrationForm.findByIdAndUpdate(
-      req.params.id,
-      { isActive: false },
-      {
-        new: true,
-      },
-    );
-    if (!result) {
-      return res.status(404).json({
-        message: `Registration form with id ${req.params.id} was not found.`,
-        data: undefined,
-        error: true,
-      });
-    }
+  const result = await RegistrationForm.findByIdAndUpdate(
+    req.params.id,
+    { isActive: false },
+    {
+      new: true,
+    },
+  );
+  if (!result) {
     return res.status(200).json({
       message: 'The registration form has been successfully deleted',
       data: result,
       error: false,
     });
-  } catch (error: any) {
-    return res.status(500).json({
-      message: `Something went wrong: ${error.message}`,
-      data: undefined,
-      error: true,
-    });
   }
+  throw new CustomError(
+    404,
+    `Registration form with id ${req.params.id} was not found.`,
+    undefined,
+  );
 };
 
 export default { getAll, getById, create, updateById, deleteById };
