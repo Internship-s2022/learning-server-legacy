07183b34d46d679bdbd4857ab871cb8945bb9606
agustinLchazarreta <agustin.chazarreta@radiumrocket.com>
2022-10-14 13:07:49 -0300

    RL-8: update flows and config

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,15 +1,15 @@
 import admin from 'firebase-admin';
 
-let firebaseApp = admin.initializeApp();
-
-!process.env.IS_TEST
-  ? (firebaseApp = admin.initializeApp({
-      credential: admin.credential.cert({
-        privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
-        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-        projectId: process.env.FIREBASE_PROJECT_ID,
-      }),
-    }))
-  : undefined;
+const firebaseApp = admin.initializeApp(
+  !process.env.IS_TEST
+    ? {
+        credential: admin.credential.cert({
+          privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+          clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+          projectId: process.env.FIREBASE_PROJECT_ID,
+        }),
+      }
+    : undefined,
+);
 
 export default firebaseApp;
