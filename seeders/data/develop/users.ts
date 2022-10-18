32d08f1b6426ae0b2dd02b4e3e517f9826f71182
iaracriscenti <iara.criscenti@radiumrocket.com>
2022-10-18 11:50:54 -0300

    RL-68: Added some fixes

--- seeders/data/develop/users.ts
+++ seeders/data/develop/users.ts
@@ -5,7 +5,7 @@ import { UserType } from '../../../src/models/user';
 const users: UserType[] = [
   {
     _id: new mongoose.Types.ObjectId('507f1f77bcf86cd799400000'),
-    postulantId: new mongoose.Types.ObjectId('1e063109a88495b45758c000'),
+    postulantId: new mongoose.Types.ObjectId('607f1f77bcf86cd799400000'),
     firebaseUid: 'M5MCzG5aCKpf0B7qpNNsi8RyjN11',
     isInternal: true,
     isActive: true,
@@ -28,6 +28,13 @@ const users: UserType[] = [
     _id: new mongoose.Types.ObjectId('507f1f77bcf86cd799400003'),
     postulantId: new mongoose.Types.ObjectId('1e063109a88495b45758c003'),
     firebaseUid: 'M5MCzG5aCKpf0B7qpNNsi8RyjN14',
+    isInternal: false,
+    isActive: true,
+  },
+  {
+    _id: new mongoose.Types.ObjectId('507f1f77bcf86cd799400002'),
+    postulantId: new mongoose.Types.ObjectId('1e063109a88495b45758c002') as unknown as string,
+    firebaseUid: 'M5MCzG5aCKpf0B7qpNNsi8RyjN13',
     isInternal: false,
     isActive: true,
   },
