9a8e8a00a77a5b596dc915691953023109e5bd2c
Radium Rocket - N 17 <guido.cerioni@radiumrocket.com>
2022-10-20 14:20:24 -0300

    RL-11: Update seeders

--- seeders/data/develop/users.ts
+++ seeders/data/develop/users.ts
@@ -5,7 +5,7 @@ import { UserType } from '../../../src/models/user';
 const users: UserType[] = [
   {
     _id: new mongoose.Types.ObjectId('507f1f77bcf86cd799400000'),
-    postulantId: new mongoose.Types.ObjectId('607f1f77bcf86cd799400000'),
+    postulantId: new mongoose.Types.ObjectId('1e063109a88495b45758c000'),
     firebaseUid: 'M5MCzG5aCKpf0B7qpNNsi8RyjN11',
     isInternal: true,
     isActive: true,
