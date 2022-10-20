12bbee6978177a14814e8a83b3f6609c187b3cdf
iaracriscenti <iara.criscenti@radiumrocket.com>
2022-10-20 18:13:00 -0300

    RL-84: Updated seeders

--- seeders/data/develop/users.ts
+++ seeders/data/develop/users.ts
@@ -12,16 +12,23 @@ const users: UserType[] = [
   },
   {
     _id: new mongoose.Types.ObjectId('507f1f77bcf86cd799400001'),
-    postulantId: new mongoose.Types.ObjectId('1e063109a88495b45758c000'),
+    postulantId: new mongoose.Types.ObjectId('1e063109a88495b45758c001'),
     firebaseUid: 'M5MCzG5aCKpf0B7qpNNsi8RyjN12',
-    isInternal: true,
+    isInternal: false,
     isActive: true,
   },
   {
     _id: new mongoose.Types.ObjectId('507f1f77bcf86cd799400002'),
-    postulantId: new mongoose.Types.ObjectId('1e063109a88495b45758c001'),
+    postulantId: new mongoose.Types.ObjectId('1e063109a88495b45758c002'),
     firebaseUid: 'M5MCzG5aCKpf0B7qpNNsi8RyjN13',
-    isInternal: true,
+    isInternal: false,
+    isActive: true,
+  },
+  {
+    _id: new mongoose.Types.ObjectId('507f1f77bcf86cd799400003'),
+    postulantId: new mongoose.Types.ObjectId('1e063109a88495b45758c003'),
+    firebaseUid: 'M5MCzG5aCKpf0B7qpNNsi8RyjN14',
+    isInternal: false,
     isActive: true,
   },
 ];
