3c64bf4b65af409f2e5a5a6cbd418499f6ec35b0
agustinLchazarreta <agustin.chazarreta@radiumrocket.com>
2022-10-25 16:43:21 -0300

    RL-160: implement qa correction

--- src/middlewares/validations.ts
+++ src/middlewares/validations.ts
@@ -1,8 +1,8 @@
 import { NextFunction, Request, Response } from 'express';
+import Joi from 'joi';
 import mongoose from 'mongoose';
 
 import { CustomError } from 'src/models/custom-error';
-
 const validateMongoID = (req: Request, res: Response, next: NextFunction) => {
   if (!req.params.id) {
     throw new CustomError(400, 'Missing mongo id parameter');
@@ -14,6 +14,24 @@ const validateMongoID = (req: Request, res: Response, next: NextFunction) => {
   return next();
 };
 
+const validateFirebaseUid = (req: Request, res: Response, next: NextFunction) => {
+  if (!req.params.uid) {
+    throw new CustomError(400, 'Missing firebase uid parameter');
+  }
+
+  const uid = Joi.string().min(28).max(36).messages({
+    'string.min': 'The firebase uid is not valid 28 characters min',
+    'string.max': 'The firebase uid is not valid 36 characters max',
+  });
+
+  const validation = uid.validate(req.params.uid);
+  if (validation.error) {
+    throw new CustomError(400, validation.error.details[0].message);
+  }
+  return next();
+};
+
 export default {
   validateMongoID,
+  validateFirebaseUid,
 };
