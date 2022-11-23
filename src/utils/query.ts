18d3fd12df0094027cbb5232ee955b6d294ed8bb
iaracriscenti <iaracriscenti@gmail.com>
2022-11-23 17:51:08 -0300

    RL-168: Updated general filter

--- src/utils/query.ts
+++ src/utils/query.ts
@@ -19,7 +19,45 @@ export const filterIncludeArrayOfIds = (includeIds: string[] | string) => {
 };
 
 export const filterByIncludes = (query: qs.ParsedQs) => {
-  return Object.entries(query).reduce((prev = {}, [key, value]) => {
+  return Object.entries(query).reduce((prev: any = {}, [key, value]) => {
+    if (key.includes('min')) {
+      const newKeys = key.split('.');
+      if (key.toLowerCase().includes('date')) {
+        return {
+          ...prev,
+          [newKeys[0]]: {
+            ...prev[newKeys[0]],
+            $gte: new Date(value as string),
+          },
+        };
+      }
+      return {
+        ...prev,
+        [newKeys[0]]: {
+          ...prev[newKeys[0]],
+          $gte: Number(value),
+        },
+      };
+    }
+    if (key.includes('max')) {
+      const newKeys = key.split('.');
+      if (key.toLowerCase().includes('date')) {
+        return {
+          ...prev,
+          [newKeys[0]]: {
+            ...prev[newKeys[0]],
+            $lt: new Date(value as string),
+          },
+        };
+      }
+      return {
+        ...prev,
+        [newKeys[0]]: {
+          ...prev[newKeys[0]],
+          $lt: Number(value),
+        },
+      };
+    }
     if (value === 'true') {
       return {
         ...prev,
