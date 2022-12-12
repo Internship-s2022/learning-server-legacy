a4811d1ca80608366bb27dcf011d7c52cdef8680
Franco Marini <franco.marini@radiumrocket.com>
2022-12-12 15:18:44 -0300

    RL-217: Auth groups on a course

--- src/routes/course/group/index.ts
+++ src/routes/course/group/index.ts
@@ -1,23 +1,47 @@
 import express from 'express';
 
+import firebaseValidations from 'src/middlewares/firebase';
 import globalValidations from 'src/middlewares/validations';
 
 import groupControllers from './controllers';
 import validations from './validations';
 
 const router = express.Router({ mergeParams: true });
+const getAccessRoleAndPermission = firebaseValidations.accessBasedOnRoleAndType({
+  roles: ['ADMIN'],
+  types: ['SUPER_ADMIN', 'NORMAL'],
+});
 
-router.get('/', groupControllers.getAll);
-router.get('/:groupId', globalValidations.validateMongoId, groupControllers.getById);
-router.post('/', validations.groupValidation, groupControllers.create);
+router.get('/', getAccessRoleAndPermission, groupControllers.getAll);
+router.get(
+  '/:groupId',
+  firebaseValidations.accessBasedOnRoleAndType({
+    roles: ['ADMIN', 'TUTOR'],
+    types: ['SUPER_ADMIN', 'NORMAL'],
+  }),
+  globalValidations.validateMongoId,
+  groupControllers.getById,
+);
+router.post('/', getAccessRoleAndPermission, validations.groupValidation, groupControllers.create);
 router.put(
   '/:groupId',
+  getAccessRoleAndPermission,
   globalValidations.validateMongoId,
   validations.groupValidation,
   groupControllers.updateById,
 );
-router.patch('/:groupId', globalValidations.validateMongoId, groupControllers.deleteById);
-router.delete('/:groupId', globalValidations.validateMongoId, groupControllers.physicalDeleteById);
-router.get('/export/csv', groupControllers.exportToCsv);
+router.patch(
+  '/:groupId',
+  getAccessRoleAndPermission,
+  globalValidations.validateMongoId,
+  groupControllers.deleteById,
+);
+router.delete(
+  '/:groupId',
+  getAccessRoleAndPermission,
+  globalValidations.validateMongoId,
+  groupControllers.physicalDeleteById,
+);
+router.get('/export/csv', getAccessRoleAndPermission, groupControllers.exportToCsv);
 
 export default router;
