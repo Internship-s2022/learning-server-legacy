4ea4f02b9fe28fd6a1799213a1c510cde6cdaf29
Guido Cerioni <guido.cerioni@radiumrocket.com>
2022-12-23 15:01:38 -0300

    RL-251: fix Module update controller

--- src/routes/course/module/controllers.ts
+++ src/routes/course/module/controllers.ts
@@ -1,10 +1,11 @@
 import { Request, Response } from 'express';
+import _ from 'lodash';
 import mongoose from 'mongoose';
 
 import { CustomError } from 'src/models/custom-error';
 import Group, { GroupType } from 'src/models/group';
 import Module, { ModuleType } from 'src/models/module';
-import { paginateAndFilter } from 'src/utils/query';
+import { filterIncludeArrayOfIds, paginateAndFilter } from 'src/utils/query';
 
 import { getModulePipeline } from './aggregations';
 
@@ -68,16 +69,98 @@ const create = async (req: Request, res: Response) => {
   }
 };
 
+const deleteModuleFromGroups = async (moduleId: string, groupsIdsToDeleteFrom: string[] = []) => {
+  try {
+    const groups = await Group.find(
+      groupsIdsToDeleteFrom.length > 0
+        ? filterIncludeArrayOfIds(groupsIdsToDeleteFrom)
+        : { modules: { $in: moduleId } },
+    );
+
+    const groupsWithoutModule: GroupType[] = groups.map((group) => ({
+      ...group.toObject(),
+      modules: group.modules.filter((_id) => _id.toString() !== moduleId),
+    }));
+
+    await Promise.all(
+      groupsWithoutModule.map(async ({ _id, ...rest }) => {
+        await Group.findByIdAndUpdate(_id, rest, {
+          new: true,
+        });
+      }),
+    );
+    return true;
+  } catch {
+    return false;
+  }
+};
+
+const addModuleToGroups = async (moduleId: string, groupsIdsToAddTo: string[]) => {
+  try {
+    const groups = await Group.find(filterIncludeArrayOfIds(groupsIdsToAddTo));
+
+    const groupsWithModule: GroupType[] = groups.map((group) => ({
+      ...group.toObject(),
+      modules: [...group.modules, new mongoose.Types.ObjectId(moduleId)],
+    }));
+
+    await Promise.all(
+      groupsWithModule.map(async ({ _id, ...rest }) => {
+        await Group.findByIdAndUpdate(_id, rest, {
+          new: true,
+        });
+      }),
+    );
+    return true;
+  } catch {
+    return false;
+  }
+};
+
 const updateById = async (req: Request, res: Response) => {
+  const moduleId = req.params.moduleId;
+
   const moduleByName = await Module.find({
     name: req.body.name,
     course: req.params.courseId,
     isActive: true,
   });
-  if (moduleByName.length) {
-    throw new CustomError(404, 'The module name already exist.');
+
+  if (moduleByName.length && moduleByName[0]._id.toString() !== moduleId) {
+    throw new CustomError(404, `The module name: "${req.body.name}" already exist.`);
   }
-  const updatedModule = await Module.findByIdAndUpdate(req.params.moduleId, req.body, {
+
+  const groupsIdsInBody: string[] = req.body?.groups;
+  const groupsInBody = await Group.find(filterIncludeArrayOfIds(groupsIdsInBody));
+
+  if (groupsIdsInBody.length !== groupsInBody.length) {
+    throw new CustomError(404, 'One or more of the the groups do not exist.');
+  }
+
+  const module = await Module.findById(moduleId);
+
+  const groupsIdsInModule: string[] = module
+    ? module?.groups.map((group) => group._id.toString())
+    : [];
+
+  const groupsIdsToDeleteFrom = _.difference(groupsIdsInModule, groupsIdsInBody);
+  const groupsIdsToAddTo = _.difference(groupsIdsInBody, groupsIdsInModule);
+
+  if (groupsIdsToDeleteFrom.length) {
+    const successDeleteFromGroups = await deleteModuleFromGroups(moduleId, groupsIdsToDeleteFrom);
+    if (!successDeleteFromGroups) {
+      throw new CustomError(500, 'There was an error while deleting the module from the groups.');
+    }
+  }
+
+  if (groupsIdsToAddTo.length) {
+    const successAddToGroups = await addModuleToGroups(moduleId, groupsIdsToAddTo);
+    if (!successAddToGroups) {
+      throw new CustomError(500, 'There was an error while adding the module to the groups.');
+    }
+  }
+
+  const updatedModule = await Module.findByIdAndUpdate(moduleId, req.body, {
     new: true,
   });
   if (updatedModule) {
@@ -87,32 +170,7 @@ const updateById = async (req: Request, res: Response) => {
       error: false,
     });
   }
-  throw new CustomError(404, `Module with id ${req.params.moduleId} was not found.`);
-};
-
-const deleteModuleFromGroups = async (moduleId: string) => {
-  try {
-    const groupsIncludeModule = await Group.find({
-      modules: { $in: moduleId },
-    });
-    const groupsWithoutModule: GroupType[] = groupsIncludeModule.map((group) => {
-      const groupWithoutModule: GroupType = {
-        ...group.toObject(),
-        modules: group.modules.filter((_id) => _id.toString() !== moduleId),
-      };
-      return groupWithoutModule;
-    });
-    await Promise.all(
-      groupsWithoutModule.map(async (group) => {
-        await Group.findByIdAndUpdate(group._id, group, {
-          new: true,
-        });
-      }),
-    );
-    return true;
-  } catch {
-    return false;
-  }
+  throw new CustomError(404, `Module with id ${moduleId} was not found.`);
 };
 
 const physicalDeleteById = async (req: Request, res: Response) => {
