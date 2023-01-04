e45b1d579ff5c51418b6ed1ee194cfcc5b27b541
Franco Marini <franco.marini@radiumrocket.com>
2023-01-04 15:45:56 -0300

    RL-285: Setup sentry and logger

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,61 @@
 import 'express-async-errors';
 import dotenv from 'dotenv';
+import * as Sentry from '@sentry/node';
+import * as Tracing from '@sentry/tracing';
 
 dotenv.config();
 
 import mongoose from 'mongoose';
 
 import app from './app';
+import logger from './config/logger';
+import errorHandler from './middlewares/error-handler';
 
 const port = process.env.PORT;
 const MONGODB_URL = process.env.MONGO_URL || '';
 
+if (process.env.ENV && process.env.SENTRY_DSN) {
+  Sentry.init({
+    dsn: process.env.SENTRY_URL,
+    environment: process.env.ENV,
+    integrations: [
+      new Sentry.Integrations.Http({ tracing: true }),
+      new Tracing.Integrations.Express({ app }),
+    ],
+    tracesSampleRate: 1.0,
+  });
+
+  app.use(Sentry.Handlers.requestHandler());
+  app.use(Sentry.Handlers.tracingHandler());
+  app.use(
+    Sentry.Handlers.errorHandler({
+      shouldHandleError(error) {
+        return (
+          !error?.status || error.status === 500 || error.status === 401 || error.status === 403
+        );
+      },
+    }),
+  );
+}
+
+app.use(errorHandler);
+
 mongoose.connect(MONGODB_URL, (error) => {
   if (error) {
-    console.log('🔴 Database error: ', error);
+    logger.log({
+      level: 'error',
+      message: '🔴 Database error: ',
+      errorData: error,
+      label: 'mongo',
+    });
   } else {
-    console.log('✅ Database connected');
+    logger.log({ level: 'info', message: '✅ Database connected', label: 'mongo' });
     app.listen(port, () => {
-      console.log(`Radium Learning server listening on port ${port}`);
+      logger.log({
+        level: 'info',
+        message: `Radium Learning server listening on port ${port}`,
+        label: 'server',
+      });
     });
   }
 });
--- src/middlewares/firebase.ts
+++ src/middlewares/firebase.ts
@@ -9,11 +9,13 @@ import CourseUser, { RoleType } from 'src/models/course-user';
 import { CustomError } from 'src/models/custom-error';
 import User from 'src/models/user';
 
-const unauthorizedMessage = 'Unauthorized. You must have permission to access.';
+const forbiddenError = new CustomError(403, 'Unauthorized. You must have permission to access.', {
+  label: 'firebase',
+});
 
 const getTokenDecoded = async (token: string) => {
   if (!token) {
-    throw new CustomError(401, 'Unauthorized. Provide a token.');
+    throw new CustomError(401, 'Unauthorized. Provide a token.', { label: 'firebase' });
   }
 
   let response: DecodedIdToken;
@@ -24,6 +26,7 @@ const getTokenDecoded = async (token: string) => {
     if (error?.errorInfo?.code === 'auth/id-token-expired') {
       throw new CustomError(401, 'Unauthorized. Firebase ID token has expired.', {
         type: 'TOKEN_EXPIRED',
+        label: 'firebase',
       });
     }
     throw new Error(error);
@@ -37,7 +40,7 @@ const superAdmin = async (req: Request, res: Response, next: NextFunction) => {
 
   const response = await getTokenDecoded(String(token));
   if (response.userType !== 'SUPER_ADMIN') {
-    throw new CustomError(403, unauthorizedMessage);
+    throw forbiddenError;
   }
 
   return next();
@@ -50,7 +53,7 @@ const normalUser = async (req: Request, res: Response, next: NextFunction) => {
   req.firebaseUid = response.uid;
 
   if (response.userType !== 'NORMAL') {
-    throw new CustomError(403, unauthorizedMessage);
+    throw forbiddenError;
   }
 
   return next();
@@ -60,19 +63,19 @@ const studentUser = async (req: Request, _res: Response, next: NextFunction) => 
   const { courseId } = req.params;
 
   if (!mongoose.Types.ObjectId.isValid(String(courseId)) || !courseId) {
-    throw new CustomError(403, unauthorizedMessage);
+    throw forbiddenError;
   }
 
   const courseDoc = await Course.findOne({ _id: courseId });
   if (!courseDoc) {
-    throw new CustomError(403, unauthorizedMessage);
+    throw forbiddenError;
   }
 
   const user = await User.findOne({ firebaseUid: req.firebaseUid });
   const courseUser = await CourseUser.findOne({ user: user?._id, course: courseId });
 
   if (!user?.isActive || courseUser?.role !== 'STUDENT') {
-    throw new CustomError(403, unauthorizedMessage);
+    throw forbiddenError;
   }
 
   req.courseUserId = String(courseUser?._id);
@@ -88,16 +91,16 @@ const accessBasedOnRoleAndType =
     const response = await getTokenDecoded(String(token));
 
     if (!types.includes(response.userType)) {
-      throw new CustomError(403, unauthorizedMessage);
+      throw forbiddenError;
     }
 
     if (response.userType === 'NORMAL') {
       if (!mongoose.Types.ObjectId.isValid(String(course)) || !course) {
-        throw new CustomError(403, unauthorizedMessage);
+        throw forbiddenError;
       }
       const courseDoc = await Course.findOne({ _id: course });
       if (!courseDoc || (req.params.courseId && req.params.courseId !== course)) {
-        throw new CustomError(403, unauthorizedMessage);
+        throw forbiddenError;
       }
 
       const user = await User.findOne({ firebaseUid: response.uid });
@@ -107,7 +110,7 @@ const accessBasedOnRoleAndType =
         !user?.isActive ||
         (courseUser && (!roles.includes(courseUser.role) || !courseUser.isActive))
       ) {
-        throw new CustomError(403, unauthorizedMessage);
+        throw forbiddenError;
       }
     }
 
